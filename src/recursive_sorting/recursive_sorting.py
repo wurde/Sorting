e06128cc2b23648b52e9274614fba9e655831d2b
Andy Bettisworth <andy@accreu.com>
2019-09-10 14:43:18 -0500

    Remove whitespace

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -6,28 +6,21 @@ def merge( arrA, arrB ):
     
     return merged_arr
 
-
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
-
     return arr
-
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # TO-DO
-
     return arr
 
 def merge_sort_in_place(arr, l, r): 
     # TO-DO
-
     return arr
-
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
 def timsort( arr ):
-
     return arr
