47f9ef2011377eb108a9c0cea8efefdf877ef7b9
Andy Bettisworth <andy@accreu.com>
2019-09-10 17:40:35 -0500

    Complete inline merge sort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -8,20 +8,40 @@ import numpy
 # Define methods
 #
 
-def merge(arrA, arrB):
-  elements = len(arrA) + len(arrB)
+def merge(left, right):
+  elements = len(left) + len(right)
   merged_arr = [0] * elements
-  # TO-DO
-  
+
   return merged_arr
 
-# TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
-  while len(arr) > 1:
-    [left, right] = merge(numpy.array_split(arr, 2))
+  if len(arr) > 1:
+    left = arr[:int(len(arr)/2)]
+    right = arr[int(len(arr)/2):]
 
     merge_sort(left)
     merge_sort(right)
+
+    i = j = k = 0
+
+    while i < len(left) and j < len(right):
+      if left[i] < right[j]:
+        arr[k] = left[i]
+        i = i + 1
+      else:
+        arr[k] = right[j]
+        j = j + 1
+      k = k + 1
+
+    while i < len(left):
+      arr[k] = left[i]
+      i = i + 1
+      k = k + 1
+
+    while j < len(right):
+      arr[k] = right[j]
+      j = j + 1
+      k = k + 1
 
   return arr
 
@@ -38,3 +58,8 @@ def merge_sort_in_place(arr, l, r):
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
 def timsort(arr):
     return arr
+
+arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+print(arr1)
+merge_sort(arr1)
+print(arr1)
